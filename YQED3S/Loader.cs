a9a7da83c38f6a99952892f8155e53f77250cc53
Flokots <florencekotohoyoh@gmail.com>
2024-04-05 13:40:39 +0200

    Define classes

---
+++ YQED3S/Loader.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YQED3S
+{
+    public class Loader
+    {
+        private readonly Parser _parser;
+
+        public Loader()
+        {
+            _parser = new Parser();
+        }
+
+        public List<Work> LoadFile(string filePath)
+        {
+            List<Work> works = new List<Work>();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+                foreach (string line in lines)
+                {
+                    string[] columns = line.Split(';');
+                    Work work = _parser.Parse(columns);
+                    works.Add(work);
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle file loading errors, e.g., file not found, invalid format
+                Console.WriteLine($"Error loading file: {ex.Message}");
+            }
+
+            return works;
+        }
+    }
+}
---
+++ YQED3S/Parser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YQED3S
+{
+    public class Parser
+    {
+        public Work Parse(string[] columns)
+        {
+            if (columns.Length == 3)
+            {
+                string name = columns[0];
+                int executionTimeMinutes = int.Parse(columns[1]);
+                int materialCost = int.Parse(columns[2]);
+
+                return new Work(name, executionTimeMinutes, materialCost);
+            }
+            else
+            {
+                throw new ArgumentException("Invalid number of columns.");
+            }
+        }
+    }
+}
